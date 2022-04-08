ca31d9d05acc53b5a12bab8136c1433416380ed0
Giulia Castro <maju.nogueir3.castr3@gmail.com>
2022-04-08 15:42:44 -0300

    first commit

---
+++ Js/Js/main.js
@@ -0,0 +1,31 @@
+
+/*
+var count;
+for (count =0; count <=5; count ++){
+    alert(count);
+};
+
+*/
+
+/*
+var count = 0;
+while (count<=5){
+    console.log(count);
+    count ++;
+ alert(count);
+
+};
+
+*/
+
+
+
+
+/*
+var idade = prompt("Qula sua idade?")
+if(idade >=18){
+    alert("maior de idade");
+} else{
+    alert("menor de idade");
+};
+*/
