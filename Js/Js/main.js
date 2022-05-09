629577d11a9d8cc9218fdcd731786726792730d8
Giulia Castro <maju.nogueir3.castr3@gmail.com>
2022-05-09 18:17:00 -0300

    Add exércicios

--- Js/Js/main.js
+++ Js/Js/main.js
@@ -1,5 +1,5 @@
 
-/*
+/*  Primeiras anotações de Js
 var count;
 for (count =0; count <=5; count ++){
     alert(count);
@@ -7,7 +7,7 @@ for (count =0; count <=5; count ++){
 
 */
 
-/*
+/*Primeiras anotações de Js
 var count = 0;
 while (count<=5){
     console.log(count);
@@ -21,7 +21,7 @@ while (count<=5){
 
 
 
-/*
+/*Primeiras anotações de Js
 var idade = prompt("Qula sua idade?")
 if(idade >=18){
     alert("maior de idade");
@@ -29,3 +29,48 @@ if(idade >=18){
     alert("menor de idade");
 };
 */
+
+
+/*Iniciando em coleções com JS  */
+
+/*Método Map */
+
+
+/*
+function getAdm (Map) {
+    let adm = [];
+for([key, value] of Map){
+    if(value === 'Adm'){
+        adm.push(key);
+    }
+}
+return adm;
+}
+
+const usuarios = new Map();
+
+usuarios.set('Luiz', 'Adm');
+usuarios.set('Rayana', 'Adm');
+usuarios.set('Leticia', 'User');
+usuarios.set('Claudia', 'Adm');
+usuarios.set('Lucimar', 'User');
+usuarios.set('Cleber', 'User');
+
+console.log(getAdm(usuarios));
+ */
+
+
+
+
+
+/*Método Set */
+const  meuArray = [30,30,40,5,223,2049,3034,5];
+
+function valoresunicos (arr){
+const mySet = new Set (arr);
+
+return [...mySet];
+
+}
+
+console.log( valoresunicos(meuArray));
